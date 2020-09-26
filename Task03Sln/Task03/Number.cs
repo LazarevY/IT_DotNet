b47fb8a67a9b7d3e0467ee2c44b98cd4c1646fc4
LazarevY <lazarev@cs.vsu.ru>
2020-09-27 00:34:18 +0300

    Add task03

---
+++ Task03Sln/Task03/Number.cs
@@ -0,0 +1,10 @@
+namespace Task03
+{
+    public abstract class Number
+    {
+        public abstract Number Add(Number other);
+        public abstract Number Sub(Number other);
+        public abstract Number Multiply(Number other);
+        public abstract Number Divide(Number other);
+    }
+}
---
+++ Task03Sln/Task03/SimpleFraction.cs
@@ -0,0 +1,94 @@
+using System;
+
+namespace Task03
+{
+    public class SimpleFraction : Number
+    {
+        private int _nominator;
+        private int _denominator;
+
+        public SimpleFraction(int nominator, int denominator)
+        {
+            if (denominator == 0)
+                throw new ArgumentException("0 cannot be used as fraction denominator!");
+            _nominator = nominator;
+            _denominator = denominator;
+        }
+
+        public int Nominator
+        {
+            get => _nominator;
+            set => _nominator = value;
+        }
+
+        public int Denominator
+        {
+            get => _denominator;
+            set => _denominator = value;
+        }
+
+        public override Number Add(Number other)
+        {
+            SimpleFraction casted = (SimpleFraction) other;
+            return new SimpleFraction(
+                _nominator * casted._denominator + casted.Nominator * _denominator,
+                this._denominator * casted._denominator).Reduced();
+        }
+
+        public override Number Sub(Number other)
+        {
+            SimpleFraction casted = (SimpleFraction) other;
+            return new SimpleFraction(
+                _nominator * casted._denominator - casted.Nominator * _denominator,
+                this._denominator * casted._denominator).Reduced();
+        }
+
+        public override Number Multiply(Number other)
+        {
+            SimpleFraction casted = (SimpleFraction) other;
+            return new SimpleFraction(
+                this._nominator * casted._nominator,
+                this._denominator * casted._denominator);
+        }
+
+        public override Number Divide(Number other)
+        {
+            SimpleFraction casted = (SimpleFraction) other;
+            return new SimpleFraction(
+                this._nominator * casted._denominator,
+                this._denominator * casted._nominator);
+        }
+
+        public void Reduce()
+        {
+            int gdc = Gdc(Math.Abs(_nominator), Math.Abs(_denominator));
+            _nominator /= gdc;
+            _denominator /= gdc;
+        }
+
+        public SimpleFraction Reduced()
+        {
+            SimpleFraction reduced = new SimpleFraction(_nominator, _denominator);
+            reduced.Reduce();
+            return reduced;
+        }
+
+        public override string ToString()
+        {
+            return $"{_nominator}/{_denominator}";
+        }
+
+        private static int Gdc(int a, int b)
+        {
+            while (true)
+            {
+                a = a % b;
+                if (a == 0)
+                    return b;
+                b = b % a;
+                if (b == 0)
+                    return a;
+            }
+        }
+    }
+}
