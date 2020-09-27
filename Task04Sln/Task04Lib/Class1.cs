cef173547427e7f88551f207fa69433c5c7d90b2
LazarevY <lazarev@cs.vsu.ru>
2020-09-27 11:41:47 +0300

    Add logic classes, and base gui

---
+++ Task04Sln/Task04Lib/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace Task04Lib
+{
+    public class Diamond
+    {
+        protected string _name;
+        protected double _weight;
+        protected int _facetingQuality;
+
+        public Diamond(string name, double weight, int facetingQuality)
+        {
+            _name = name;
+            _weight = weight;
+            _facetingQuality = facetingQuality;
+        }
+
+        public virtual double Quality() => 0.4 * _weight + 0.6 * _facetingQuality;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public double Weight
+        {
+            get => _weight;
+            set => _weight = value;
+        }
+
+        public int FacetingQuality
+        {
+            get => _facetingQuality;
+            set => _facetingQuality = value;
+        }
+    }
+}
---
+++ Task04Sln/Task04Lib/ColorizedDiamond.cs
@@ -0,0 +1,42 @@
+namespace Task04Lib
+{
+    public enum DiamondColor
+    {
+        White, Blue, Yellow
+    }
+    public class ColorizedDiamond : Diamond
+    {
+
+        private DiamondColor _color;
+        
+        public ColorizedDiamond(
+            string name, 
+            double weight, 
+            int facetingQuality, 
+            DiamondColor color) 
+            : 
+            base(name, weight, facetingQuality)
+        {
+            _color = color;
+        }
+
+        public DiamondColor Color
+        {
+            get => _color;
+            set => _color = value;
+        }
+
+        public override double Quality()
+        {
+            switch (_color)
+            {
+                case DiamondColor.Blue:
+                    return base.Quality() + 1;
+                case DiamondColor.Yellow:
+                    return base.Quality() - 0.5;
+                default:
+                    return base.Quality();
+            }
+        }
+    }
+}
