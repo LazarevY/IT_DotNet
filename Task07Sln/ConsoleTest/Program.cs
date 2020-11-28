92692c5f31624b38cb860fd50bb0fc7d89b06749
LazarevY <lazarev@cs.vsu.ru>
2020-11-28 13:24:37 +0300

    Add reflection utils

---
+++ Task07Sln/ConsoleTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechnicsLib;
+using Reflections = ReflectionsUtils.Reflections;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Reflections r = new Reflections(
+                "/home/lazarev/RiderProjects/IT_Tasks/IT_DotNet/Task06Sln/TechnicsLib/bin/Debug/TechnicsLib.dll");
+            foreach (var type in r.AllImplementsOf(typeof(ITechnics)))
+            {
+                Console.WriteLine(type.ToString());
+            }
+
+            Console.WriteLine(typeof(VideoPlayer).IsAssignableFrom(typeof(ITechnics)));
+        }
+    }
+}
---
+++ Task07Sln/ReflectionsUtils/Reflections.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ReflectionsUtils
+{
+    public class Reflections
+    {
+        public string PathToScan { get; }
+        private Assembly Assembly { get; set; }
+
+        public Reflections(string pathToScan)
+        {
+            PathToScan = pathToScan;
+            Assembly = Assembly.LoadFrom(PathToScan);
+        }
+
+        public List<Type> AllImplementsOf(Type type)
+        {
+            if (!type.IsInterface)
+                return new List<Type>();
+            return new List<Type>(Assembly.GetTypes()
+                .Where(t => UsualClass(t) && InheritsFrom(t, type)));
+        }
+
+        public bool UsualClass(Type type)
+        {
+            return type.IsClass && !type.IsAbstract;
+        }
+        public bool InheritsFrom(Type type, Type baseType)
+        {
+            // null does not have base type
+            if (type == null)
+            {
+                return false;
+            }
+
+            // only interface or object can have null base type
+            if (baseType == null)
+            {
+                return type.IsInterface || type == typeof(object);
+            }
+
+            // check implemented interfaces
+            if (baseType.IsInterface)
+            {
+                return type.GetInterfaces().Contains(baseType);
+            }
+
+            // check all base types
+            var currentType = type;
+            while (currentType != null)
+            {
+                if (currentType.BaseType == baseType)
+                {
+                    return true;
+                }
+
+                currentType = currentType.BaseType;
+            }
+
+            return false;
+        }
+    }
+}
