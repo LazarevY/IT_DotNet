acef6ac0f8bee1a00624c0835144ad7c5ecd7c3f
LazarevY <lazarev@cs.vsu.ru>
2020-12-01 13:32:06 +0300

    Add player add and remove functions

--- Task06Sln/Task06GUI/MainWindow.cs
+++ Task06Sln/Task06GUI/MainWindow.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Gtk;
 using TechnicsLib;
 using UI = Gtk.Builder.ObjectAttribute;
@@ -15,10 +16,16 @@ namespace Task06GUI
         [UI] private Button _offButton;
         [UI] private TextView _guiConsoleTextView;
         [UI] private Label _infoLabel;
+        [UI] private Box _videoCodecsInContainer;
+        [UI] private Box _audioCodecsInContainer;
+        [UI] private SpinButton _ramInput;
+        [UI] private Entry _serailInput;
 
         private List<ITechnics> _technicses;
         private ITechnics _currentPlayer;
         private int _index = 0;
+        private List<CheckButton> _vidCodecsChoices = new List<CheckButton>();
+        private List<CheckButton> _audioCodecsChoices = new List<CheckButton>();
 
         public MainWindow() : this(new Builder("MainWindow.glade"))
         {
@@ -30,31 +37,29 @@ namespace Task06GUI
 
             DeleteEvent += Window_DeleteEvent;
 
-            _technicses =  new List<ITechnics>();
+            _technicses = new List<ITechnics>();
             AddPlayers();
         }
 
         private void AddPlayers()
         {
-            _technicses.Add(new VideoPlayer("SN1625", 1024, 
-                new HashSet<VideoPlayer.VideoCodecs>{VideoPlayer.VideoCodecs.H_264},
-                new HashSet<VideoPlayer.AudioCodecs>{VideoPlayer.AudioCodecs.MP3}));
-            _technicses.Add(new VideoPlayer("SN1623", 1024, 
-                new HashSet<VideoPlayer.VideoCodecs>{VideoPlayer.VideoCodecs.H_264,VideoPlayer.VideoCodecs.H_265},
-                new HashSet<VideoPlayer.AudioCodecs>{VideoPlayer.AudioCodecs.MP3, VideoPlayer.AudioCodecs.WMA}));
-            _technicses.Add(new VideoPlayer("SN1645", 2048, 
-                new HashSet<VideoPlayer.VideoCodecs>{VideoPlayer.VideoCodecs.H_264,VideoPlayer.VideoCodecs.H_265},
-                new HashSet<VideoPlayer.AudioCodecs>{VideoPlayer.AudioCodecs.MP3, VideoPlayer.AudioCodecs.WMA}));
-            _technicses.Add(new VideoPlayer("SN16789", 4096, 
-                new HashSet<VideoPlayer.VideoCodecs>{VideoPlayer.VideoCodecs.H_264,VideoPlayer.VideoCodecs.H_265, VideoPlayer.VideoCodecs.TS},
-                new HashSet<VideoPlayer.AudioCodecs>{VideoPlayer.AudioCodecs.MP3, VideoPlayer.AudioCodecs.WMA}));
-            _technicses.Add(new VideoPlayer("SN162345", 4096, 
-                new HashSet<VideoPlayer.VideoCodecs>{VideoPlayer.VideoCodecs.H_264,VideoPlayer.VideoCodecs.H_265, VideoPlayer.VideoCodecs.TS, VideoPlayer.VideoCodecs.MKV},
-                new HashSet<VideoPlayer.AudioCodecs>{VideoPlayer.AudioCodecs.MP3, VideoPlayer.AudioCodecs.WMA, VideoPlayer.AudioCodecs.FLAC}));
-            setCurrentPlayer(_technicses[0]);
+            _technicses.Add(new VideoPlayer("SN16789", 4096,
+                new HashSet<VideoPlayer.VideoCodecs>
+                    {VideoPlayer.VideoCodecs.H_264, VideoPlayer.VideoCodecs.H_265, VideoPlayer.VideoCodecs.TS},
+                new HashSet<VideoPlayer.AudioCodecs> {VideoPlayer.AudioCodecs.MP3, VideoPlayer.AudioCodecs.WMA}));
+            _technicses.Add(new VideoPlayer("SN162345", 4096,
+                new HashSet<VideoPlayer.VideoCodecs>
+                {
+                    VideoPlayer.VideoCodecs.H_264, VideoPlayer.VideoCodecs.H_265, VideoPlayer.VideoCodecs.TS,
+                    VideoPlayer.VideoCodecs.MKV
+                },
+                new HashSet<VideoPlayer.AudioCodecs>
+                    {VideoPlayer.AudioCodecs.MP3, VideoPlayer.AudioCodecs.WMA, VideoPlayer.AudioCodecs.FLAC}));
+            SetCurrentPlayer(_technicses[0]);
+            InitFields();
         }
 
-        private void setCurrentPlayer(ITechnics player)
+        private void SetCurrentPlayer(ITechnics player)
         {
             _currentPlayer = player;
             _enabledStatusLabel.Text = player.Enabled ? "Enabled" : "Disabled";
@@ -63,16 +68,16 @@ namespace Task06GUI
 
         private void ToPreviousPlayer(object? sender, EventArgs eventArgs)
         {
-            if (--_index < 0)
-                _index = _technicses.Count - 1;
-            setCurrentPlayer(_technicses[_index]);
+            --_index;
+            UpdateIndexPolicy();
+            SetCurrentPlayer(_technicses[_index]);
         }
 
         private void ToNextPlayer(object? sender, EventArgs eventArgs)
         {
-            if (++_index >= _technicses.Count)
-                _index = 0;
-            setCurrentPlayer(_technicses[_index]);
+            ++_index;
+            UpdateIndexPolicy();
+            SetCurrentPlayer(_technicses[_index]);
         }
 
         private void TurnOn(object? sender, EventArgs eventArgs)
@@ -87,10 +92,99 @@ namespace Task06GUI
             _enabledStatusLabel.Text = "Disabled";
         }
 
+        private void InitFields()
+        {
+            foreach (var widget in _videoCodecsInContainer.Children)
+            {
+                _videoCodecsInContainer.Remove(widget);
+            }
+
+            foreach (var widget in _audioCodecsInContainer.Children)
+            {
+                _audioCodecsInContainer.Remove(widget);
+            }
+
+            _videoCodecsInContainer.PackStart(new Label("Video codecs"), false, true, 3);
+            _audioCodecsInContainer.PackStart(new Label("Audio codecs"), false, true, 3);
+
+            foreach (var value in Enum.GetNames(typeof(VideoPlayer.VideoCodecs)))
+            {
+                CheckButton b = new CheckButton(value);
+                _vidCodecsChoices.Add(b);
+                _videoCodecsInContainer.PackStart(b, false, true, 3);
+            }
+
+            foreach (var value in Enum.GetNames(typeof(VideoPlayer.AudioCodecs)))
+            {
+                CheckButton b = new CheckButton(value);
+                _audioCodecsChoices.Add(b);
+                _audioCodecsInContainer.PackStart(b, false, true, 3);
+            }
+
+            _videoCodecsInContainer.ShowAll();
+            _audioCodecsInContainer.ShowAll();
+        }
+
+        private void AddPlayer(object? sender, EventArgs eventArgs)
+        {
+            var vidSet = new HashSet<VideoPlayer.VideoCodecs>(_vidCodecsChoices
+                .Where(b => b.Active)
+                .Select(b => Enum.Parse<VideoPlayer.VideoCodecs>(b.Label)));
+            var audSet = new HashSet<VideoPlayer.AudioCodecs>(_audioCodecsChoices
+                .Where(b => b.Active)
+                .Select(b => Enum.Parse<VideoPlayer.AudioCodecs>(b.Label)));
+
+            if (vidSet.Count == 0)
+                vidSet.Add(VideoPlayer.VideoCodecs.H_264);
+            if (audSet.Count == 0)
+                audSet.Add(VideoPlayer.AudioCodecs.MP3);
+
+            string sn = _serailInput.Text.Equals("") ? "DEFAULT" : _serailInput.Text;
+
+            _technicses.Add(new VideoPlayer(
+                    sn,
+                    _ramInput.ValueAsInt,
+                    vidSet,
+                    audSet
+            ));
+            SetCurrentPlayer(_technicses[_index]);
+        }
+
+        private void RemovePlayer(object? sender, EventArgs eventArgs)
+        {
+            if (_technicses.Count == 0)
+            {
+                return;
+            }
+
+            _technicses.RemoveAt(_index);
+            if (_technicses.Count == 0)
+            {
+                _enabledStatusLabel.Text = "";
+                _infoLabel.Text = "";
+                UpdateIndexPolicy();
+                return;
+            }
+            SetCurrentPlayer(_technicses[_index]);
+        }
+
+        private void UpdateIndexPolicy()
+        {
+            if (_technicses.Count == 0)
+            {
+                _index = 0;
+                return;
+            }
+            if (_index >= _technicses.Count)
+                _index = 0;
+            if (_index < 0)
+                _index = _technicses.Count - 1;
+        }
+
+
         private void Window_DeleteEvent(object sender, DeleteEventArgs a)
         {
             Application.Quit();
         }
-        
     }
 }
